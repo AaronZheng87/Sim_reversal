ecdd60d198375e9ab4b59a37b134a75b27c9c51c
Yuan-Rui Zheng <74256146+AaronZheng87@users.noreply.github.com>
2024-03-28 20:07:33 +0100

    first

---
+++ 1_Script/datakeras.py
@@ -0,0 +1,7 @@
+import pandas as pd
+from skimage.color import rgb2hed
+import skimage
+import numpy as np
+from skimage.io import imread
+from skimage.color import rgba2rgb
+from skimage.transform import resize
